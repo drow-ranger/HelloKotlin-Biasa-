df9229b93ed2ef5ed8bfa5a612a1117fb1d048a6
drow-ranger <nico98deo@gmail.com>
2018-08-31 13:53:04 +0700

    latian Extension

--- app/src/main/java/com/deonico/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/deonico/hellokotlin/MainActivity.kt
@@ -6,13 +6,17 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.TextView
 import android.widget.Toast
+import com.deonico.hellokotlin.R.array.club_image
+import com.deonico.hellokotlin.R.array.club_name
+import com.deonico.hellokotlin.R.layout.activity_main
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var items: MutableList<Item> = mutableListOf()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    //BIASA
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -26,6 +30,19 @@ class MainActivity : AppCompatActivity() {
             toast.show()
         }
 
+    }*/
+
+    //EXTENSION
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+        initData()
+
+        club_list.layoutManager = LinearLayoutManager(this)
+        club_list.adapter = RecyclerViewAdapter(this, items){
+            val toast = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 
     private fun initData(){
@@ -40,5 +57,4 @@ class MainActivity : AppCompatActivity() {
         //Recycle the typed array
         image.recycle()
     }
-
 }
--- app/src/main/java/com/deonico/hellokotlin/RecyclerViewAdapter.kt
+++ app/src/main/java/com/deonico/hellokotlin/RecyclerViewAdapter.kt
@@ -8,8 +8,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.deonico.hellokotlin.R.layout.item_list
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.*
 
-class RecyclerViewAdapter (private val context: Context, private val  items: List<Item>, private val listener: (Item) -> Unit)
+//BIASA
+/*class RecyclerViewAdapter (private val context: Context, private val  items: List<Item>, private val listener: (Item) -> Unit)
     : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
@@ -26,15 +30,40 @@ class RecyclerViewAdapter (private val context: Context, private val  items: Lis
         private val name = view.findViewById<TextView>(R.id.name)
         private val image = view.findViewById<ImageView>(R.id.image)
 
-        /*fun bindItem(items: Item) {
+        *//*fun bindItem(items: Item) {
             name.text = items.name
             Glide.with(itemView.context).load(items.image).into(image)
-        }*/
+        }*//*
 
         fun bindItem(items: Item, listener: (Item) -> Unit) {
             name.text = items.name
             Glide.with(itemView.context).load(items.image).into(image)
             itemView.setOnClickListener { listener(items) }
+        }
+    }
+}*/
+
+//Extension
+class RecyclerViewAdapter(private val context: Context, private val items: List<Item>, private val listener: (Item) -> Unit)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ViewHolder(LayoutInflater.from(context).inflate(item_list, parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(items[position], listener)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+
+    class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+            LayoutContainer {
+
+        fun bindItem(items: Item, listener: (Item) -> Unit) {
+            name.text = items.name
+            Glide.with(containerView).load(items.image).into(image)
+            containerView.setOnClickListener { listener(items) }
         }
     }
 }
